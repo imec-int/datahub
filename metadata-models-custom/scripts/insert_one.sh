24dedfc2995b35b5254cabf1b625b46c83c45072
Adrien Schautteet <schaut82@imec.be>
2023-05-12 16:20:17 +0200

    Add data contract

--- metadata-models-custom/scripts/insert_one.sh
+++ metadata-models-custom/scripts/insert_one.sh
@@ -1,1 +1,3 @@
-datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,logging_events,PROD)" --aspect customDataQualityRules --aspect-data data/dq_rule.json
+datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,default.anpr_tf_devs,PROD)" --aspect customDataQualityRules --aspect-data scripts/data/dq_rule.json
+
+datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,default.anpr_tf_devs,PROD)" --aspect dataContract --aspect-data scripts/data/data_contract.json
